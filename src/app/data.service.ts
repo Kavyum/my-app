8f214432e21da12ef7a64541f8197f1351ccb0a9
Kavyum <iuri.cavalaria.90@gmail.com>
2018-11-12 18:56:56 +0000

    master branch upload

---
+++ src/app/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers()
+  {
+    return this.http.get('/api/users')
+  }
+
+  getUser(userId)
+  {
+    
+    return this.http.get('/api/users/' +userId)
+  
+  }
+
+  getPosts()
+  {
+    
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  
+  }
+
+}
